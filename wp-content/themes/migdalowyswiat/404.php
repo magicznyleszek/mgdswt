0fdfd9e4f2244f967ffc39be7529726d0ed5cbc9
Leszek Pietrzak <lpietrzak@gog.com>
2014-12-19 18:02:17 +0100

    reorganized twigs, added some images

--- wp-content/themes/migdalowyswiat/404.php
+++ wp-content/themes/migdalowyswiat/404.php
@@ -1,8 +1,5 @@
-<?php get_header(); ?>
-	<div id="content">
-		<article>
-			<h1>Przepraszamy, strony nie znaleziono.</h1>
-			<?php get_template_part('partials/sitemap'); ?>
-		</article>
-	</div>
-<?php get_footer(); ?>
+<?php
+    $context = Timber::get_context();
+    $context['posts'] = Timber::get_posts();
+    Timber::render('404.twig', $context);
+?>
