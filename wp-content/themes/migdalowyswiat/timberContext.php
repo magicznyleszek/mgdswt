7e978d85197798c051c8397da9c39bced292ff8d
Leszek Pietrzak <lpietrzak@gog.com>
2014-12-20 20:42:57 +0100

    searchbox and text inputs

--- wp-content/themes/migdalowyswiat/timberContext.php
+++ wp-content/themes/migdalowyswiat/timberContext.php
@@ -5,7 +5,23 @@ function add_to_context($context){
     /* So here you are adding data to Timber's context object, i.e... */
     $context['glowneMenu'] = new TimberMenu();
 
-    $context['socialMenu'] = '';
+    $context['socialMenu'] = array(
+        array(
+            'title' => 'Mail',
+            'url' => 'mailto:test@test.com',
+            'icon' => 'envelope-black'
+        ),
+        array(
+            'title' => 'Pinterest',
+            'url' => 'http://pinterest.com',
+            'icon' => 'pinterest-black'
+        ),
+        array(
+            'title' => 'Instagram',
+            'url' => 'http://instagram.com',
+            'icon' => 'instagram-black'
+        )
+    );
 
     return $context;
 }
