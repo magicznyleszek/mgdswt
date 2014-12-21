c8553284a8dd82ab15683652e339fcc86b2d2c9c
Leszek Pietrzak <lpietrzak@gog.com>
2014-12-21 15:36:52 +0100

    posts thumbnails

--- wp-content/themes/migdalowyswiat/timberContext.php
+++ wp-content/themes/migdalowyswiat/timberContext.php
@@ -3,12 +3,14 @@
 add_filter('timber_context', 'add_to_context');
 function add_to_context($context){
 
+    // for assets verstion number
     $context['cacheTimestamp'] = time();
 
-    $context['glowneMenu'] = new TimberMenu(2);
-
+    // favourites menu (used in sidebar and on favourites page)
     $context['ulubioneMenu'] = new TimberMenu(7);
 
+    // used in app header
+    $context['glowneMenu'] = new TimberMenu(2);
     $context['socialMenu'] = array(
         array(
             'title' => 'Mail',
@@ -26,6 +28,9 @@ function add_to_context($context){
             'icon' => 'instagram-black'
         )
     );
+
+    // list of categories
+    $context['categories'] = Timber::get_terms('category');
 
     return $context;
 }
