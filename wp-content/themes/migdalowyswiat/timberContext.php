1cc9dafafdc4abcb28926740e7e89f9efa9424b9
johny <jan@neatcoding.com>
2015-05-10 17:26:37 +0200

    using post #65 for sidebar O mnie section

--- wp-content/themes/migdalowyswiat/timberContext.php
+++ wp-content/themes/migdalowyswiat/timberContext.php
@@ -3,28 +3,31 @@
 add_filter('timber_context', 'add_to_context');
 function add_to_context($context){
 
-    // for assets verstion number
+    // for assets version number
     $context['cacheTimestamp'] = time();
 
     // favourites menu (used in sidebar and on favourites page)
     $context['ulubioneMenu'] = new TimberMenu(7);
+
+    // for sidebar "O mnie" section
+    $context['aboutMePost'] = new TimberPost(65);
 
     // used in app header
     $context['glowneMenu'] = new TimberMenu(2);
     $context['socialMenu'] = array(
         array(
             'title' => 'Mail',
-            'url' => 'mailto:test@test.com',
+            'url' => 'mailto:migdalowyswiat@gmail.com',
             'icon' => 'envelope-black'
         ),
         array(
             'title' => 'Pinterest',
-            'url' => 'http://pinterest.com',
+            'url' => 'https://pl.pinterest.com/migdalowyswiat/',
             'icon' => 'pinterest-black'
         ),
         array(
             'title' => 'Instagram',
-            'url' => 'http://instagram.com',
+            'url' => 'https://instagram.com/migdalowyswiat/',
             'icon' => 'instagram-black'
         )
     );
